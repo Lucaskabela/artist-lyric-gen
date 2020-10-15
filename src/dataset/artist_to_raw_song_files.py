e29644f2ca8360e1f909dcb2bcb72683b8542a2e
Bill Yang <bill@Bills-MBP.attlocal.net>
2020-10-14 20:20:45 -0500

    old changes, now not needed since we changes get_songs

--- src/dataset/artist_to_raw_song_files.py
+++ src/dataset/artist_to_raw_song_files.py
@@ -1,24 +1,24 @@
-import argparse
 import json
-from dataset_utils import loop_and_process
+from dataset_utils import loop_and_process, name_to_file_name
+from get_songs import artist_lyric_dir
+from tqdm import tqdm
 
 raw_songs_dir = 'raw_songs'
-raw_songs_file_prefix = 'Raw_Song'
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--name", type=str, help="Name of an artist")
-    parser.add_argument("--path", type=str, help="Path to the artist json file")
-    args = parser.parse_args()
-    with open(args.path) as jsonfile:
-        artist = json.load(jsonfile)
-        songs = artist["songs"]
-        def process_song(song):
-            return {
-                'title': song['title'],
-                'artist': song['primary_artist']['name'],
-                'lyrics': song['lyrics']
-            }
-        def get_song_name(song):
-            return song['title']
-        loop_and_process(songs, process_song, "Song", get_song_name, "{}/{}".format(raw_songs_dir, raw_songs_file_prefix))
+def artist_to_raw_song_files(artists_file):
+    with open(artists_file) as openfile:
+        artists = openfile.readlines()
+    artists = [artist.strip() for artist in artists]
+    for artist_name in tqdm(artists):
+        with open("{}/{}".format(artist_lyric_dir, name_to_file_name(artist_name))) as jsonfile:
+            artist = json.load(jsonfile)
+            songs = artist["songs"]
+            def process_song(song):
+                return {
+                    'title': song['title'],
+                    'artist': song['primary_artist']['name'],
+                    'lyrics': song['lyrics']
+                }
+            def get_song_name(song):
+                return song['title']
+            loop_and_process(songs, process_song, "Song", get_song_name, raw_songs_dir)
