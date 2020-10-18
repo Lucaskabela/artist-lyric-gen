aa97cdbdffe293c1a04ce5d3a3799674d309289d
Lucaskabela <lucaskabela@utexas.edu>
2020-10-17 21:38:14 -0500

    Add base network for saving and loading

--- src/models.py
+++ src/models.py
@@ -9,27 +9,47 @@ import torch.nn as nn
 from os import path
 
 
-class GhostLSTM(nn.Module):
+class BaseNetwork(nn.Module):
+    def __init__(self):
+        super(BaseNetwork, self).__init__()
+
+    def device(self):
+        return next(self.parameters()).device
+
+    def save_model(self):
+        file_name = "{}.th".format(self.name)
+        fn = path.join("ckpt", file_name)
+        return torch.save(
+            self.state_dict(),
+            path.join(path.dirname(path.abspath(__file__)), fn),
+        )
+
+    def load_model(self):
+        file_name = "{}.th".format(self.name)
+        fn = path.join("ckpt", file_name)
+        self.load_state_dict(
+            torch.load(
+                path.join(path.dirname(path.abspath(__file__)), fn),
+                map_location=self.device(),
+            )
+        )
+
+
+class GhostLSTM(BaseNetwork):
     """
     Defines the LSTM model based onGhostWriter Potash et al. 2015
     """
 
-    def __init__(self, vocab_size, embedding_dim, hidden_size):
+    def __init__(self, vocab_size, embedding_dim, hidden_size, name="ghost"):
 
         super().__init__()
         self.vocab_size = vocab_size
-
+        self.name = name
         self.embedding = nn.Embedding(self.vocab_size, embedding_dim)
         nn.init.uniform_(self.embedding.weight, -0.1, 0.1)
         # TODO: add args for
         self.encoder = nn.LSTM(embedding_dim, hidden_size)
         self.decoder = nn.LSTM(hidden_size, self.vocab_size)
-
-    def device(self):
-        if next(self.parameters()).is_cuda:
-            return torch.device("cuda")
-        else:
-            return torch.device("cpu")
 
     def forward(self, context):
         """
@@ -40,29 +60,15 @@ class GhostLSTM(nn.Module):
         pass
 
 
-def save_model(model):
-    """
-    Saves the model to a specified location
-    """
-    if isinstance(model, GhostLSTM):
-        return torch.save(
-            model.state_dict(),
-            path.join(path.dirname(path.abspath(__file__)), "ghost.th"),
-        )
-    raise ValueError("model type '%s' not supported!" % str(type(model)))
+class CVAE(BaseNetwork):
+    def __init__(self, vocab_size, embedding_dim, hidden_size, name="cvae"):
 
+        super().__init__()
+        self.vocab_size = vocab_size
+        self.name = name
 
-def load_model(model):
-    """
-    Loads a saved model, depending on the type of model
-    """
-    if isinstance(model, GhostLSTM):
-        model.load_state_dict(
-            torch.load(
-                path.join(path.dirname(path.abspath(__file__)), "ghost.th"),
-                map_location=model.device(),
-            )
-        )
-        return model
-    else:
-        raise ValueError("model type '%s' not supported!" % str(type(model)))
+        self.embedding = nn.Embedding(self.vocab_size, embedding_dim)
+        nn.init.uniform_(self.embedding.weight, -0.1, 0.1)
+        # TODO: add args for
+        self.encoder = nn.LSTM(embedding_dim, hidden_size)
+        self.decoder = nn.LSTM(hidden_size, self.vocab_size)
