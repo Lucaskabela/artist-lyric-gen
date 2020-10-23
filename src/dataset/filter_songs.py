714634e8f3336d64c47daa639d7079f3d1d736f1
Bill Yang <bill@Bills-MBP.attlocal.net>
2020-10-23 10:37:20 -0500

    filter songs on language and verse count

--- src/dataset/filter_songs.py
+++ src/dataset/filter_songs.py
@@ -2,25 +2,42 @@ from langdetect import detect, DetectorFactory, lang_detect_exception
 DetectorFactory.seed = 0
 import json
 from dataset_utils import loop_and_process, name_to_file_name, read_list_from_file, write_list_to_file
-from get_songs import raw_songs_dir
+from clean_verses import cleaned_verses_dir
 
 filtered_songs_dir = "filtered_songs"
 
-def filter_song(song):
+def filter_lyrics(lyrics, title, bar):
     try:
-        lang = detect(song['lyrics'])
+        lang = detect(lyrics.strip())
     except:
-        print("{} has no features".format(song['title']))
+        bar.write("IIII {} has no features".format(title))
         return True
     if lang != 'en':
-        print("{} is {}".format(song['title'], lang))
+        bar.write("OOOO {} has {}".format(title, lang))
         return True
     return False
 
-def process_song(song):
-    with open("{}/{}.json".format(raw_songs_dir, name_to_file_name(song))) as song_file:
+
+def filter_song(song, bar):
+    if len(song['verses']) == 0:
+        bar.write("XXXX {} has no verses".format(song['title']))
+        return True
+    if len(song['verses']) > 15:
+        bar.write("XXXX {} has too many verses".format(song['title']))
+        return True
+    all_lyrics = ''
+    for verse in song['verses']:
+        all_lyrics = all_lyrics + verse['lyrics']
+    # cant use a song with another language in it
+    if filter_lyrics(all_lyrics, song['title'], bar):
+        return True
+    return False
+
+
+def process_song(song, bar):
+    with open("{}/{}.json".format(cleaned_verses_dir, name_to_file_name(song))) as song_file:
         song = json.load(song_file)
-    if filter_song(song):
+    if filter_song(song, bar):
         return False
     return None
 
