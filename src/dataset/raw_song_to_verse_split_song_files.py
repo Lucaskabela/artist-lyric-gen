12d3b3ca80a8ff7ec22e4dbc87374d709530d924
Bill Yang <bill@Bills-MBP.attlocal.net>
2020-10-23 10:36:34 -0500

    fixed bug in adding metadata

--- src/dataset/raw_song_to_verse_split_song_files.py
+++ src/dataset/raw_song_to_verse_split_song_files.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 from dataset_utils import loop_and_process, name_to_file_name
-from get_songs import raw_songs_dir
+from clean_lyrics import cleaned_songs_dir
 
 verse_split_songs_dir = "raw_verse_songs"
 
@@ -9,21 +9,24 @@ def raw_songs_to_verse_split_songs(song_list_path, dir_prefix):
     with open(song_list_path) as listfile:
         song_list = listfile.readlines()
     song_list = [song.strip() for song in song_list]
-    def process_song(song):
-        with open("{}/{}.json".format(raw_songs_dir, name_to_file_name(song))) as song_file:
+    def process_song(song, bar):
+        with open("{}/{}.json".format(cleaned_songs_dir, name_to_file_name(song))) as song_file:
             song = json.load(song_file)
         verses = []
         lyrics = song['lyrics']
         i = 0
         verse_lyrics = ''
         verse_metadata = ''
+        def write_verse(v_metadata, v_lyrics, verses):
+            if len(v_metadata.strip()) > 0:
+                verses.append({'metadata': v_metadata, 'lyrics': v_lyrics})
+            return verses
         while i < len(lyrics):
             # Parse the songs into each verse
             if lyrics[i] == '[':
                 # we reached a new verse
                 # append the previous verse if there was one
-                if len(verse_metadata) > 0 and len(verse_lyrics.strip()) > 0 and "produce" not in verse_metadata.lower():
-                    verses.append({'metadata': verse_metadata, 'lyrics': verse_lyrics})
+                verses = write_verse(verse_metadata, verse_lyrics, verses)
                 # reset for the new verse
                 verse_lyrics = ''
                 verse_metadata = ''
@@ -31,9 +34,12 @@ def raw_songs_to_verse_split_songs(song_list_path, dir_prefix):
                 while i < len(lyrics) and lyrics[i] != ']':
                     verse_metadata = verse_metadata + lyrics[i]
                     i = i + 1
+                if i < len(lyrics):
+                    verse_metadata = verse_metadata + lyrics[i]
             else:
                 verse_lyrics = verse_lyrics + lyrics[i]
             i = i + 1
+        verses = write_verse(verse_metadata, verse_lyrics, verses)
         return {
             'title': song['title'],
             'verses': verses,
