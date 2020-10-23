f57356079f49656c6f8498d57079d77cc52cbabc
Bill Yang <bill@Bills-MBP.attlocal.net>
2020-10-23 10:39:32 -0500

    fix metadata parsing, change bad metadata output list

--- src/dataset/verse_songs_extract_artists.py
+++ src/dataset/verse_songs_extract_artists.py
@@ -1,11 +1,11 @@
 import json
 import re
 from dataset_utils import loop_and_process, name_to_file_name, read_list_from_file, write_list_to_file
-from raw_song_to_verse_split_song_files import verse_split_songs_dir
+from clean_verses import cleaned_verses_dir
 
 verse_artists_dir = "verses_with_artists"
-artists_list_file = "ARTISTS_LIST.txt"
-bad_metadata_file = "BAD_METADATA.txt"
+artists_list_file = "_ARTISTS_LIST.txt"
+bad_metadata_file = "_BAD_METADATA.txt"
 
 def get_artists_from_metadata(song_title, metadata, song_artist, featured_artists):
     def write_out_error(song_title, metadata):
@@ -16,15 +16,18 @@ def get_artists_from_metadata(song_title, metadata, song_artist, featured_artist
     if s[0] == "[":
         s = s[1:]
     if s[-1] == "]":
-        s = s[:2]
+        s = s[:-1]
     # metadata should have the for <verse_type> : <artist1> & <artist2>...
     artists = s.split(':')
     if len(artists) > 2:
         write_out_error(song_title, metadata)
     elif len(artists) < 2:
+        # we dont want to write out things that were like [verse] or [verse 1]
+        cleaned_metadata = re.sub(r'[0-9]+', '', artists[0])
+        if ' ' in cleaned_metadata.strip():
+            write_out_error(song_title, metadata)
         # no artists were given so we default to the main artist
         artists = [song_artist]
-        write_out_error(song_title, metadata)
     else:
         artists = artists[1]
         artists = artists.replace(')','')
@@ -36,8 +39,8 @@ def get_artists_from_metadata(song_title, metadata, song_artist, featured_artist
     write_list_to_file(artists, "{}/{}".format(verse_artists_dir, artists_list_file), 'a')
     return list(artists)
 
-def process_song(song):
-    with open("{}/{}.json".format(verse_split_songs_dir, name_to_file_name(song))) as song_file:
+def process_song(song, bar):
+    with open("{}/{}.json".format(cleaned_verses_dir, name_to_file_name(song))) as song_file:
         song = json.load(song_file)
     for verse in song['verses']:
         artists = get_artists_from_metadata(song['title'], verse['metadata'], song['artist'], song['featured_artists'])
