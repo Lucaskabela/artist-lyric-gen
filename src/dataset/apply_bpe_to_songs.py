24d6f3ca9b83faeebccc3da383df06461239dd5b
Bill Yang <bill@Bills-MBP.attlocal.net>
2020-11-02 15:09:30 -0600

    added bpe processing to dataset_utils

--- src/dataset/apply_bpe_to_songs.py
+++ src/dataset/apply_bpe_to_songs.py
@@ -1,19 +1,13 @@
 from subword_nmt import apply_bpe
 import json
 import codecs
-from dataset_utils import loop_and_process, name_to_file_name, read_list_from_file, write_list_to_file, remove_duplicates_from_list_file, clean_artist_names
+from dataset_utils import loop_and_process, name_to_file_name, read_list_from_file, write_list_to_file, remove_duplicates_from_list_file, clean_artist_names, get_bpe_object, apply_bpe_to_string
 from fix_tokens import verses_with_tokens
 
 bpe_songs_dir = 'bpe_songs'
 
 def run_bpe_on_songs(codes_file, song_list_path, out_dir):
-    codes = codecs.open(codes_file, encoding='utf-8')
-    # codes: codes file opened
-    # merges: -1 default, apply learned merge operations
-    # separator: @@ default
-    # vocabulary: None, default
-    # glossaries: None default
-    bpe = apply_bpe.BPE(codes)
+    bpe = get_bpe_object(codes_file)
     song_list = read_list_from_file(song_list_path)
     def process_song(song, bar):
         with open("{}/{}.json".format(verses_with_tokens, name_to_file_name(song))) as song_file:
@@ -21,7 +15,7 @@ def run_bpe_on_songs(codes_file, song_list_path, out_dir):
         for verse in song['verses']:
             if verse['valid']:
                 lyrics = verse['lyrics']
-                lyrics = bpe.process_line(lyrics)
+                lyrics = apply_bpe_to_string(lyrics, bpe)
                 verse['lyrics'] = lyrics
         return song
     loop_and_process(song_list, process_song, "Song", lambda x:x, out_dir)
